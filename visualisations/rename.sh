f651d07e810bf380955edc588ea8085ab4afadcf
suzannenie <nie.suzanne01@gmail.com>
2021-09-13 12:14:10 -0400

    axial slices

--- visualisations/rename.sh
+++ visualisations/rename.sh
@@ -1,11 +1,8 @@
 COUNTER=0
-cd sub-860/coronal;
+cd sub-860/axial;
 for file in *.png
 do 
     [ -f "$file" ] && echo mv "$file" "${COUNTER}.png";
     mv "$file" "${COUNTER}.png";
     let COUNTER++;
 done
-
-for i in range(18):
-    print("![image](visualisations/sub-860/coronal/{}.png".format(i) + ")")
